cdb68fe2171b0ad56b3a9e694691650385c98ea5
Matthias Hoelling <mhoelling@gmail.com>
2019-07-13 22:29:14 +0000

    with install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+#install special lirc
+/usr/bin/apt-get remove lirc
+/usr/bin/dpkg -i *.deb
+
+confpath=/etc/lirc/lircd.conf.d
+sysdpath=/etc/systemd/system
+
+#install configs and systemd
+rm -v "$confpath/*.conf"
+cp -v configs/*.conf $confpath
+cp -v systemd/*service $sysdpath
+
+/bin/systemctl reenable lircd.service
+/bin/systemctl reenable lircd-lirc1.service
+
+#build modules
+make
+make modules_install
+depmod
+
+/usr/sbin/armbian-add-overlay gpio-ook-tx-overlay.dts
+/usr/sbin/armbian-add-overlay pwm-ir-tx-overlay.dts
+
+echo "please reboot"
+
+exit
